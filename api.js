4c8c464bff0cd633799789c85c1ca0944b1d4304
aramaisay <aramaisyeghiazaryan@gmail.com>
2020-07-15 15:27:36 +0400

    Devided into dif functions

--- api.js
+++ api.js
@@ -8,20 +8,39 @@ const getAllCountries = async () => {
 const PlaceAllCountries = async () => {
     const data = await getAllCountries();
     data.forEach(element => {
-        const newFlag = document.createElement('div');
-        newFlag.classList.add('flag');
-        newFlag.addEventListener('click',() => { getDataAboutCountry(element['alpha2Code']) })
-        const flagImg = document.createElement('img');
-        flagImg.classList.add('flagImg');
-        flagImg.src = element.flag;
-        newFlag.append(flagImg);
-        const flagDetails = document.createElement('div');
-        flagDetails.classList.add('flagDetails');
-        flagDetails.innerText = `${element.name} click to get more info`;
-        newFlag.append(flagDetails);
+        // Creating the flag container
+        const newFlag = createNewFlagElement(element,'flag');
+        // Adding new flag image
+        createNewFlagImgElement(element,newFlag,'flagImg');
+        // Adding theDetails
+        createNewFlagElementDetail(element,newFlag,'flagDetails');
+
         const parentNode = document.getElementById('flags');
         parentNode.append(newFlag);
     })
+}
+
+const createNewFlagElement = (element, className) => {
+    const newFlag = document.createElement('div');
+    newFlag.classList.add(className);
+    newFlag.addEventListener('click',() => { getDataAboutCountry(element['alpha2Code']) })
+    return newFlag;
+}
+
+const createNewFlagImgElement = (element,newFlag,className) => {
+    const flagImg = document.createElement('img');
+    flagImg.classList.add(className);
+    flagImg.src = element.flag;
+    newFlag.append(flagImg);
+    return newFlag;
+}
+
+const createNewFlagElementDetail = (element,newFlag,className) => {
+    const flagDetails = document.createElement('div');
+    flagDetails.classList.add(className);
+    flagDetails.innerText = `${element.name} click to get more info`;
+    newFlag.append(flagDetails);
+    return newFlag;
 }
 
 const getDataAboutCountry = (code) => {
